1be11c6728795fa02d5edb566f05c6321941998e
lchorolque <leandrochorolque@gmail.com>
2022-09-08 13:05:50 -0300

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+async function req() {
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  console.log(num1, num2);
+
+  url = "http://localhost:4500/calc/?num1=" + num1 + "&num2=" + num2;
+  console.log(url);
+  const options = {
+    method: "GET",
+    headers: { "Access-Control-Allow-Origin": "*" },
+  };
+
+  const response = await fetch(url, options).then((response) => {
+    return response.json();
+  });
+
+  document.getElementById("add").value = response.ADDITION;
+  document.getElementById("sub").value = response.SUB;
+  document.getElementById("mul").value = response.MULTIPLICATION;
+  document.getElementById("div").value = response.DIV;
+}
